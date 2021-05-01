cc25a14ce3f38fb75c1f22c36e48516bec418864
Nicolas <n.bertho.web@gmail.com>
2021-05-01 10:06:55 +0200

    [API] App setup

--- server/app.js
+++ server/app.js
@@ -4,14 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// REGISTER ROUTERS
+const indexRouter = require('./routes/index');
+const usersRoute = require('./routes/users');
 
+
+// APP¨CONFIG
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,22 +19,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// ERROR HANDLER
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({error: true, msg: err.message, content: {}});
 });
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// MIDDLEWARES
+router.use((req, res, next) => {
+  console.log('REQUEST MADE TO /api/users ROUTE');
+  next();
+})
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({error: false, msg: 'Users root URL', content: {}});
+})
+
 
 module.exports = router;
